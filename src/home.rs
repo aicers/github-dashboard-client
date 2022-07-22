72319f822bbd2dce1f5b2227fdc199ef57e58d16
Junggon Kim <jgkim@einsis.com>
2022-07-22 11:08:51 +0900

    [WIP] Server query
    어케하지
    
    Close #4

--- src/home.rs
+++ src/home.rs
@@ -1,4 +1,10 @@
+//use std::sync::mpsc::channel;
+
+//use gloo_net::http::Request;
 use yew::prelude::*;
+
+//const URL: &str = "127.0.0.1:8000";
+//const QUERY: &str = "{\"query\": \"{ issues }\"}";
 
 pub enum Message {}
 
@@ -22,8 +28,38 @@ impl Component for Model {
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div>
-                <p>{ "AICE GitHub Dashboard" }</p>
+            <p>
+            { "AICE GitHub Dashboard" }
+            //{ self.home_view(ctx)}
+            </p>
             </div>
         }
     }
 }
+/*
+impl Model {
+    fn home_view(&self, _ctx: &Context<Self>) -> Html {
+        let (tx, rx) = channel();
+        let mut uri = URL.to_string();
+        uri.push_str("/graphql");
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let post_str = Request::post(&uri)
+                .header("Content-Type", "application/json")
+                .body(QUERY)
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+            tx.send(post_str).unwrap();
+        });
+        let res_str = rx.recv().unwrap();
+
+        html! {
+            <div>{res_str}</div>
+        }
+    }
+}
+*/
